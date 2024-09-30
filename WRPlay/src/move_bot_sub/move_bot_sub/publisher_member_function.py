3e6bf89b9d640581d5da121a6b2d1f0abf1ec5b0
schromya <schromya@gmail.com>
2024-09-30 15:38:17 -0500

    Finalized communication process and tuned/scale P of PID

--- WRPlay/src/move_bot_sub/move_bot_sub/publisher_member_function.py
+++ WRPlay/src/move_bot_sub/move_bot_sub/publisher_member_function.py
@@ -29,8 +29,6 @@ class Controller(Node):
         self.ir_array = [math.inf for _ in range(180)] 
         self.power_left = 0.0
         self.power_right = 0.0
-        self.continue_move = True
-        self.stopped = False
         timer_period = 0.01  # seconds
         
     
@@ -55,9 +53,9 @@ class Controller(Node):
             10)
         
         
-    def send_light_request(self):
+    def send_light_request(self, status):
         req = LightStatus.Request()
-        req.light_status = 0
+        req.light_status = status
         self.future = self.status_cli.call_async(req)
         #rclpy.spin_until_future_complete(self, self.future)
 
@@ -67,45 +65,36 @@ class Controller(Node):
         future = self.continue_client.call_async(req)
         
         def handle_response(future):
-            # Find next beacon when notified
-            # if future.result().can_continue:
-                #self.get_logger().info('Continuation: "%s"' % future.result())
-            self.continue_move = future.result().can_continue
+            if future.result().can_continue:
+                self.send_light_request(1)
 
         future.add_done_callback(handle_response)
 
 
     def drive_callback(self):
         msg = DrivePower()
-        msg.left_power = self.power_left # 1.0
-        msg.right_power = self.power_right # 1.0 #'{left_power: , right_power: 10}'
-
+        msg.left_power = self.power_left
+        msg.right_power = self.power_right
         self.drive_publisher.publish(msg)
 
     def beacon_callback(self, msg):
-        #self.get_logger().info('I heard: "%s"' % msg.distances)
         self.ir_array = msg.distances
 
-        # Stop when arrive at beacon
+        # Stop if within beacon distance
         if any(distance < 100 for distance in self.ir_array): 
             self.power_left = 0.0
             self.power_right = 0.0
-            self.stopped = True
-            self.get_logger().info("HERE2")
-            self.send_light_request()
+            self.send_light_request(0)
         # Spin when no beacon signals are picked up
         elif all(distance == math.inf for distance in self.ir_array):
-            self.get_logger().info("HERE3")
             self.power_left = -5.0
             self.power_right = 5.0
 
         # Drive to beacon when have signal
         else: 
-            self.get_logger().info("HERE4")
+            MAX_SPEED = 5.0
+            MIN_SPEED = -5.0
             beacon_index = next(index for index, value in enumerate(self.ir_array) if value != math.inf)
-            MAX_SPEED = 10.0
-            MIN_SPEED = -10.0
-            
             self.power_left = MAX_SPEED if beacon_index > 90 else self.scale_value(beacon_index, 0, 90, MIN_SPEED, MAX_SPEED)
             self.power_right = MAX_SPEED if beacon_index < 90 else self.scale_value(beacon_index, 90, 180, MAX_SPEED, MIN_SPEED)
 
